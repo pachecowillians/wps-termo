06d91a681846cced96dde8f518e897fbfaeb4e8a
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-14 19:14:26 -0300

    Creating keyboard

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,10 @@
 import type { NextPage } from 'next'
+import { Fragment } from 'react';
+import KeyboardLetter from '../components/KeyboardLetter/KeyboardLetter'
 import styles from '../styles/Home.module.css'
+
+var letters = "QWERTYUIOPASDFGHJKLZXCVBNM";
+var lettersArray = letters.split('');
 
 const Home: NextPage = () => {
   return (
@@ -9,7 +14,23 @@ const Home: NextPage = () => {
           <span>WPS - TERMO</span>
         </header>
         <main className={styles.main}></main>
-        <footer className={styles.footer}></footer>
+        <footer className={styles.footer}>
+          {lettersArray.map(letter => {
+            if (letter == 'L') {
+              return <Fragment>
+                <KeyboardLetter letter={letter} />
+                <KeyboardLetter letter={'backspace'} />
+              </Fragment>
+            } else if (letter == 'M') {
+              return <Fragment>
+                <KeyboardLetter letter={letter} />
+                <KeyboardLetter letter={'enter'} />
+              </Fragment>
+            } else {
+              return <KeyboardLetter letter={letter} />
+            }
+          })}
+        </footer>
       </div>
     </div>
   )
