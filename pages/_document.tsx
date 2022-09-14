ea98b0a5d38a96b25b3e5873a0594569975a88e3
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-14 18:16:14 -0300

    Adding title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -13,6 +13,7 @@ class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@600&display=swap" rel="stylesheet" />
+                    <title>WPS - Termo</title>
                 </Head>
                 <body>
                     <Main />
