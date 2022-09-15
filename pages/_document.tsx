ed005262c7ba70c1b99f8e64747ad9d37e601e64
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-15 17:48:51 -0300

    Adding favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -14,6 +14,9 @@ class MyDocument extends Document {
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Mitr:wght@400;500;600&display=swap" rel="stylesheet" />
                     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,700,0,200" />
+                    <link rel="icon" href="/icon.png" />
+                    <link rel="shortcut icon" href="/icon.png" />
+                    <link rel="apple-touch-icon" href="/icon.png" />
                 </Head>
                 <body>
                     <Main />
