3ca8a1185321f56131667c43e53759e3918a37a1
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-20 08:40:16 -0300

    Updating cell selected status on KeyDown

--- pages/index.tsx
+++ pages/index.tsx
@@ -48,9 +48,24 @@ const Home: NextPage = () => {
         if (isLetter(letter)) {
             setMatrix(prevMatrix =>
                 prevMatrix.map(line => line.map(
-                    cell => cell.position.line == activeLine && cell.position.column == activeColumn ?
-                        { ...cell, letter: letter } :
-                        cell))
+                    cell => {
+                        if (cell.position.line == activeLine && cell.position.column == activeColumn) {
+                            return {
+                                ...cell,
+                                letter: letter,
+                                status: 'active'
+                            }
+                        } else if (cell.position.line == activeLine && cell.position.column == activeColumn + 1) {
+                            return {
+                                ...cell,
+                                status: 'selected'
+                            }
+                        } else {
+
+                            return cell
+                        }
+                    })
+                )
             )
             setActiveColumn(prevActiveLine =>
                 (prevActiveLine + 1) < 5 ?
