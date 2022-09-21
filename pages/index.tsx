322e78d2d21cc5acec616d34c107a2465e117308
Willian Pacheco Silva <willianpacheco31@gmail.com>
2022-09-21 11:21:05 -0300

    Removing next Image component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import type { NextPage } from 'next'
-import Image from 'next/image';
 import { useEffect, useRef, useState } from 'react';
 import Alert from '../components/Alert/Alert';
 import GameOverModal from '../components/GameOverModal/GameOverModal';
@@ -364,10 +363,10 @@ const Home: NextPage = () => {
                                     </div>
                                     <div className={styles.contacts}>
                                         <span>Contact me:</span>
-                                        <a href='https://www.linkedin.com/in/pachecowillians' target='_blank' rel="noreferrer"><Image src="linkedin.svg" alt="LinkedIn" /> linkedin.com/in/pachecowillians</a>
-                                        <a href='https://github.com/pachecowillians' target='_blank' rel="noreferrer"><Image src="github.svg" alt="GitHub" /> github.com/pachecowillians</a>
-                                        <a href='mailto:willianpacheco31@gmail.com' target='_blank' rel="noreferrer"><Image src="gmail.svg" alt="Gmail" /> willianpacheco31@gmail.com</a>
-                                        <a href='https://pachecowillians.github.io' target='_blank' rel="noreferrer"><Image src="portfolio.svg" alt="Portfolio" /> pachecowillians.github.io</a>
+                                        <a href='https://www.linkedin.com/in/pachecowillians' target='_blank' rel="noreferrer"><img src="linkedin.svg" alt="LinkedIn" /> linkedin.com/in/pachecowillians</a>
+                                        <a href='https://github.com/pachecowillians' target='_blank' rel="noreferrer"><img src="github.svg" alt="GitHub" /> github.com/pachecowillians</a>
+                                        <a href='mailto:willianpacheco31@gmail.com' target='_blank' rel="noreferrer"><img src="gmail.svg" alt="Gmail" /> willianpacheco31@gmail.com</a>
+                                        <a href='https://pachecowillians.github.io' target='_blank' rel="noreferrer"><img src="portfolio.svg" alt="Portfolio" /> pachecowillians.github.io</a>
                                     </div>
                                 </div>
                                 <div className={styles.modalFooter}>
